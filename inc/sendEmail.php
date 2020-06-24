1879aed9a9f2f9c03519a0f59992a91260211fb4
Othmane Bakhtaoui <uo259323@uniovi.es>
2020-06-25 01:49:41 +0200

    Update sendEmail.php

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -27,7 +27,7 @@ if($_POST) {
 
 
    // Set Message
-   $message .= "Email from: " . $name . "<br />";
+   $message = "Email from: " . $name . "<br />";
    $message .= "Email address: " . $email . "<br />";
    $message .= "Message: <br />";
    $message .= $contact_message;
@@ -37,10 +37,10 @@ if($_POST) {
    $from =  $name . " <" . $email . ">";
 
    // Email Headers
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $email . "\r\n";
- 	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+   $headers = "From: " . $from . "\r\n";
+   $headers .= "Reply-To: ". $email . "\r\n";
+   $headers .= "MIME-Version: 1.0\r\n";
+   $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
 
    if (!$error) {
